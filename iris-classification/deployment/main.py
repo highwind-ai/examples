c15f0f8450ccd880e9005aac19bf88edf7ac1a61
Ruan Pretorius <ruan@melio.ai>
2024-05-06 15:41:36 +0200

    Format with black and isort

--- iris-classification/deployment/main.py
+++ iris-classification/deployment/main.py
@@ -1,12 +1,20 @@
 import argparse
-from typing import Dict
-from kserve import Model, ModelServer, model_server, InferRequest, InferOutput, InferResponse
-from kserve.utils.utils import generate_uuid
-import os
-import pandas as pd
-import joblib
 import json
+import os
+from typing import Dict
+
+import joblib
 import numpy as np
+import pandas as pd
+from kserve import (
+    InferOutput,
+    InferRequest,
+    InferResponse,
+    Model,
+    ModelServer,
+    model_server,
+)
+from kserve.utils.utils import generate_uuid
 
 ARTIFACT_SAVE_DIR = "./saved_model/"
 
@@ -27,7 +35,9 @@ class MyModel(Model):
         self.model = joblib.load(os.path.join(ARTIFACT_SAVE_DIR, "model.joblib"))
         self.ready = True
 
-    def preprocess(self, payload: InferRequest, headers: Dict[str, str] = None) -> np.ndarray:
+    def preprocess(
+        self, payload: InferRequest, headers: Dict[str, str] = None
+    ) -> np.ndarray:
         # Get data from payload
         infer_input = payload.inputs[0]
         raw_data = np.array(infer_input.data)
@@ -38,18 +48,23 @@ class MyModel(Model):
         print(f"** scaled_data ({type(scaled_data)}): {scaled_data}")
         return scaled_data
 
-    def predict(self, data: np.ndarray, headers: Dict[str, str] = None) -> InferResponse:
+    def predict(
+        self, data: np.ndarray, headers: Dict[str, str] = None
+    ) -> InferResponse:
         response_id = generate_uuid()
         result = self.model.predict(data)
-        infer_output = InferOutput(name="output-0", shape=list(result.shape), datatype="INT64", data=result)
-        infer_response = InferResponse(model_name=self.name, infer_outputs=[infer_output], response_id=response_id)
+        infer_output = InferOutput(
+            name="output-0", shape=list(result.shape), datatype="INT64", data=result
+        )
+        infer_response = InferResponse(
+            model_name=self.name, infer_outputs=[infer_output], response_id=response_id
+        )
         return infer_response
+
 
 parser = argparse.ArgumentParser(parents=[model_server.parser])
 parser.add_argument(
-    "--model_name",
-    default="model",
-    help="The name that the model is served under."
+    "--model_name", default="model", help="The name that the model is served under."
 )
 args, _ = parser.parse_known_args()
 
